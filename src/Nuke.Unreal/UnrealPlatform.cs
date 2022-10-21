4b25532d09f48eb8bab77d19fd40a66686aae799
David Mórász <david.morasz@realworld-one.com>
2022-10-21 23:49:36 +0200

    move platform flags to UnrealPlatform.cs

--- src/Nuke.Unreal/UnrealPlatform.cs
+++ src/Nuke.Unreal/UnrealPlatform.cs
@@ -7,6 +7,24 @@ using Nuke.Common.Tooling;
 
 namespace Nuke.Unreal
 {
+    [Flags]
+    public enum UnrealPlatformFlag
+    {
+        Win64 =         0b_00000001,
+        Win32 =         0b_00000010,
+        HoloLens =      0b_00000100,
+        Mac =           0b_00001000,
+        Linux =         0b_00010000,
+        LinuxAArch64 =  0b_00100000,
+        Android =       0b_01000000,
+        IOS =           0b_10000000,
+        AllWin =        Win32  | Win64,
+        AllLinux =      Linux  | LinuxAArch64,
+        AllDesktop =    AllWin | AllLinux | Mac
+
+        // TODO: rest of the platforms
+        // Engine/Source/Programs/UnrealBuildTool/Configuration/UEBuildTarget.cs
+    }
 
     [TypeConverter(typeof(TypeConverter<UnrealPlatform>))]
     public class UnrealPlatform : Enumeration
