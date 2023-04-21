2c90c3931942ca01fb75042cf43eeb4fc532e26c
David Mórász <david.morasz@realworld-one.com>
2023-04-21 18:00:48 +0200

    merge together multiple invokations of subtools

--- src/Nuke.Unreal/Tools/UnrealAutomationToolConfig.cs
+++ src/Nuke.Unreal/Tools/UnrealAutomationToolConfig.cs
@@ -7,5 +7,35 @@ namespace Nuke.Unreal.Tools;
 
 public class UnrealAutomationToolConfig : UnrealAutomationToolConfigGenerated
 {
-    public override string Gather() => string.Join(' ', Arguments);
+    public override string Gather()
+    {
+        var output = new List<string>();
+        var notool = new List<string>();
+        var perTool = new Dictionary<string, List<string>>();
+        foreach(var carg in Arguments)
+        {
+            var args = carg.Trim().Split(' ');
+            if (Configs.Any(c => args[0] == c.Name))
+            {
+                if (!perTool.TryGetValue(args[0], out var toolArgs))
+                {
+                    toolArgs = new();
+                    perTool.Add(args[0], toolArgs);
+                }
+                toolArgs.AddRange(args.Skip(1));
+            }
+            else
+            {
+                notool.AddRange(args);
+            }
+        }
+
+        foreach((var tool, var args) in perTool)
+        {
+            output.Add(tool);
+            output.AddRange(args);
+        }
+        output.AddRange(notool);
+        return string.Join(' ', output);
+    }
 }
