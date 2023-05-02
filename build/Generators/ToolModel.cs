6f3e3ef1f68bae6c0ea7ca51b8cc77b58f9b2a67
David Mórász <david.morasz@realworld-one.com>
2023-05-02 17:20:53 +0200

    + ToolModel.Merge

--- build/Generators/ToolModel.cs
+++ build/Generators/ToolModel.cs
@@ -13,5 +13,30 @@ public class ToolModel : CommandLineEntity, IProvideArguments
     public TypeModel ConfigType { init; get; }
     public string ClassKeywords { get; set; } = "";
     public List<ArgumentModel> Arguments { get; set; } = new();
-    public List<ToolModel> Subtools { get; } = new();
+    public List<ToolModel> Subtools { get; private set; } = new();
+
+    public ToolModel Merge(ToolModel other)
+    {
+        Compatibility = Compatibility.Union(other.Compatibility).ToList();
+        this.MergeDocs(other);
+
+        foreach(var arg in other.Arguments)
+        {
+            this.AddArgument(arg);
+        }
+
+        foreach(var tool in other.Subtools)
+        {
+            var outputSubtool = Subtools.FirstOrDefault(t => t.ConfigName == tool.ConfigName);
+            if (outputSubtool == null)
+            {
+                Subtools.Add(tool);
+            }
+            else
+            {
+                outputSubtool.Merge(tool);
+            }
+        }
+        return this;
+    }
 }
