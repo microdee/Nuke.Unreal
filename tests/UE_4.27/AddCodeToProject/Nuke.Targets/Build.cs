d54b2f120f44c31a8c09e5578155bf32b08b39e7
David Mórász <m@mcro.de>
2023-05-29 16:10:56 +0200

    added Test targets base class

--- tests/UE_4.27/AddCodeToProject/Nuke.Targets/Build.cs
+++ tests/UE_4.27/AddCodeToProject/Nuke.Targets/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
@@ -6,13 +7,14 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Git;
 using Nuke.Common.Utilities.Collections;
 using Nuke.Unreal;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 
-class Build : UnrealBuild
+class Build : UnrealBuildTest
 {
     /// Support plugins are available for:
     ///   - JetBrains ReSharper        https://nuke.build/resharper
@@ -24,4 +26,15 @@ class Build : UnrealBuild
 
     Target Test => _ => _
         .Triggers(AddCode, Generate);
+
+    protected override void PostBuildCheck()
+    {
+        var projectContent = File.ReadAllText(ProjectPath);
+        Assert.DirectoryExists(ProjectFolder / "Source" / ProjectName);
+        var targetPath = ProjectFolder / "Source" / (ProjectName + ".Target.cs");
+        Assert.FileExists(targetPath);
+        var targetContent = File.ReadAllText(targetPath);
+        Assert.True(targetContent.Contains($"\"{ProjectName}\""));
+        Assert.True(projectContent.Contains(ProjectName));
+    }
 }
