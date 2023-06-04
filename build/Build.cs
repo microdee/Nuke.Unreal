fb016de8d49071cd271f611863223732baa0acfb
David Mórász <m@mcro.de>
2023-06-04 21:43:04 +0200

    added "latest" compatibilities for future-proofing

--- build/Build.cs
+++ build/Build.cs
@@ -89,7 +89,7 @@ class Build : NukeBuild
                 new("4.27", UnrealCompatibility.UE_4),
                 new("5.0", UnrealCompatibility.UE_5_0),
                 new("5.1", UnrealCompatibility.UE_5_1),
-                new("5.2", UnrealCompatibility.UE_5_2),
+                new("5.2", UnrealCompatibility.UE_5_Latest),
             };
             new UbtGeneratorFromSource().Generate(this, engines);
             new UatGenerator().Generate(this, engines);
