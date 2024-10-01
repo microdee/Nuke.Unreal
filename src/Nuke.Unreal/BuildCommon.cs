7f8262ed40ac7564cc7d5d81e239e16aa1875650
David Morasz <m@mcro.de>
2024-10-01 20:13:29 +0200

    get owning module, plugin or project of a path

--- src/Nuke.Unreal/BuildCommon.cs
+++ src/Nuke.Unreal/BuildCommon.cs
@@ -38,24 +38,42 @@ public static class BuildCommon
         Assert.NotNullOrEmpty(contentFolder, "Couldn't find contents folder of Nuke.Unreal.");
         return contentFolder!;
     }
-    
-    public static IEnumerable<AbsolutePath> SubTreeProject(this AbsolutePath origin, Func<AbsolutePath, bool> filter = null) =>
-        origin.SubTree(sd => filter?.Invoke(sd) ?? true
-            && !sd.Name.StartsWith(".")
-            && !sd.Name.StartsWith("Nuke.")
-            && sd.Name != "Intermediate"
-            && sd.Name != "Binaries"
-            && sd.Name != "ThirdParty"
-            && sd.Name != "Saved"
-        );
 
-    public static bool LookAroundFor(Func<string, bool> predicate, out AbsolutePath result) =>
-        PathExtensions.LookAroundFor(predicate, out result, sd =>
-            !sd.Name.StartsWith(".")
-            && !sd.Name.StartsWith("Nuke.")
-            && sd.Name != "Intermediate"
-            && sd.Name != "Binaries"
-            && sd.Name != "ThirdParty"
-            && sd.Name != "Saved"
-        );
+    public static bool IsProjectFolder(this AbsolutePath path)
+        => !path.Name.StartsWith('.')
+        && !path.Name.StartsWith("Nuke.")
+        && path.Name != "Intermediate"
+        && path.Name != "Binaries"
+        && path.Name != "ThirdParty"
+        && path.Name != "Saved";
+    
+    public static IEnumerable<AbsolutePath> SubTreeProject(this AbsolutePath origin, Func<AbsolutePath, bool>? filter = null) =>
+        origin.SubTree(sd => filter?.Invoke(sd) ?? true && sd.IsProjectFolder());
+
+    public static bool LookAroundFor(Func<string, bool> predicate, out AbsolutePath? result) =>
+        PathExtensions.LookAroundFor(predicate, out result, IsProjectFolder);
+
+    public static AbsolutePath? GetOwner(this AbsolutePath path, string ownerPattern)
+        => path
+            .DescendantsAndSelf(d => d.Parent, d => Path.GetPathRoot(d) != d)
+            .SelectMany(p => p.GlobFiles(ownerPattern))
+            .FirstOrDefault();
+
+    public static AbsolutePath? GetOwningPlugin(this AbsolutePath path)
+        => path
+            .DescendantsAndSelf(d => d.Parent, d => Path.GetPathRoot(d) != d)
+            .SelectMany(p => p.GlobFiles("*.uplugin"))
+            .FirstOrDefault();
+
+    public static AbsolutePath? GetOwningProject(this AbsolutePath path)
+        => path
+            .DescendantsAndSelf(d => d.Parent, d => Path.GetPathRoot(d) != d)
+            .SelectMany(p => p.GlobFiles("*.uproject"))
+            .FirstOrDefault();
+
+    public static AbsolutePath? GetOwningModule(this AbsolutePath path)
+        => path
+            .DescendantsAndSelf(d => d.Parent, d => Path.GetPathRoot(d) != d)
+            .SelectMany(p => p.GlobFiles("*.Build.cs"))
+            .FirstOrDefault();
 }
