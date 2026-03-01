71723aeeba02701e8dc0d1081ec610fe1fa40f5c
David Morasz <m@mcro.de>
2026-03-02 00:58:47 +0100

    + VersionExtensions

---
+++ src/Nuke.Unreal/VersionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nuke.Unreal;
+
+public static class VersionExtensions
+{
+    public static string ToStringAutoFieldCount(this Version version, bool allowZero = false)
+    {
+        var tolerance = allowZero ? 0 : -1;
+        var fields = 4;
+        if (version.Revision <= tolerance) --fields;
+        if (version.Build <= tolerance) --fields;
+        if (version.Minor <= tolerance) --fields;
+        return version.ToString(fields);
+    }
+
+}
